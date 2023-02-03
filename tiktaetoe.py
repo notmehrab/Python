95e446e8fe2c97b2dc238fa65a2fb8d0a4cfbaf7
unknown <2btc11@IESRAMONLLULL.LOCAL>
2023-02-03 09:51:31 +0100

    voy bastante bien con el juego

--- tiktaetoe.py
+++ tiktaetoe.py
@@ -1,17 +1,42 @@
 import random
 
-#Elegir opciones
-user = "X"
-pc = "O"
+
+#Crear la tabla
+table = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+lista =[0,1,2,3,4,5,6,7,8]
 
 while True:
-    #Crear la tabla
-    table = ["", "", "", "", "", "", "", "", "",]
-    lista =[0,1,2,3,4,5,6,7,8,9]
-
     #Pedir Casilla al usuario
-    position = int(input("elige uno: "))
-    table[position] = "x"
+    userp = int(input("elige una posición entre 0 a 8: "))
+
+    #user 
+    if table[userp] == " " :
+        table[userp] = "X"
+        lista.remove(userp)
+    
+    if table[0] and table[1] and table[2] == "X":
+        print("has ganado")
+        break
+    if table[3] and table[4] and table[5] == "X":
+        print("has ganado")
+        break
+    if table[6] and table[7] and table[8] == "X":
+        print("has ganado")
+        break
+
+    if table[0] and table[3] and table[6] == "X":
+        print("has ganado")
+        break
+    if table[1] and table[4] and table[7] == "X":
+        print("has ganado")
+        break
+    if table[2] and table[5] and table[8] == "X":
+        print("has ganado")
+        break
+    
+    if table[0] and table[4] and table[8] == "X":
+        print("has ganado")
+        break
 
     print(
     "| "+table[0]+"| "+table[1]+"| "+table[2]+"|\n"
@@ -19,18 +44,15 @@ while True:
     "| "+table[6]+"| "+table[7]+"| "+table[8]+"|\n"
     )
 
-    if position == lista:
-        remove.postion(lista)
+    #ordenador
+    pcp = random.choice(lista)
+    table[pcp] = "O"
+    lista.remove(pcp)
 
-    #Eleccion del ordenador
-  
-    pc_position = random.choice(lista)
-    table[pc_position] = "O"
+    print(lista)
 
     print(
     "| "+table[0]+"| "+table[1]+"| "+table[2]+"|\n"
     "| "+table[3]+"| "+table[4]+"| "+table[5]+"|\n"
     "| "+table[6]+"| "+table[7]+"| "+table[8]+"|\n"
     )
-    if pc_position == lista:
-        remove.postion(lista)
