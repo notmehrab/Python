ea26c40fc8992fe8459c7b0a497c9010daff2cbc
unknown <2btc11@IESRAMONLLULL.LOCAL>
2023-02-06 09:51:43 +0100

    He acabado el juego pero lo estoy mejorando

--- tiktaetoe.py
+++ tiktaetoe.py
@@ -1,58 +1,107 @@
 import random
 
+#Choose option
+
+user_option = input("elige X o O: ").capitalize()  #user decision
+
+if user_option == "X":
+    pc_option = "O"   #pc decision
+elif user_option == "O":
+    pc_option = "X"  #pc decision
 
 #Crear la tabla
+
 table = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
 lista =[0,1,2,3,4,5,6,7,8]
 
+#Reglas
+
+
 while True:
-    #Pedir Casilla al usuario
+#Pedir Casilla al usuario
+
     userp = int(input("elige una posición entre 0 a 8: "))
 
-    #user 
+#user 
+
     if table[userp] == " " :
-        table[userp] = "X"
+        table[userp] = user_option
         lista.remove(userp)
-    
-    if table[0] and table[1] and table[2] == "X":
-        print("has ganado")
-        break
-    if table[3] and table[4] and table[5] == "X":
-        print("has ganado")
-        break
-    if table[6] and table[7] and table[8] == "X":
-        print("has ganado")
-        break
+    else:
+        print("¡La casilla está seleccionada. Por favor, elige otra!")
+        userp = int(input("elige una posición entre 0 a 8: "))
 
-    if table[0] and table[3] and table[6] == "X":
-        print("has ganado")
-        break
-    if table[1] and table[4] and table[7] == "X":
-        print("has ganado")
-        break
-    if table[2] and table[5] and table[8] == "X":
-        print("has ganado")
-        break
-    
-    if table[0] and table[4] and table[8] == "X":
-        print("has ganado")
-        break
-
+#Display decision
     print(
     "| "+table[0]+"| "+table[1]+"| "+table[2]+"|\n"
     "| "+table[3]+"| "+table[4]+"| "+table[5]+"|\n"
     "| "+table[6]+"| "+table[7]+"| "+table[8]+"|\n"
     )
 
-    #ordenador
+#User rules
+
+    if table[0] == table[1] == table[2] == user_option:
+        print("has ganado")
+        break
+    if table[3] == table[4] == table[5] == user_option:
+        print("has ganado")
+        break
+    if table[6] == table[7] == table[8] == user_option:
+        print("has ganado")
+        break
+
+    if table[0] == table[3] == table[6] == user_option:
+        print("has ganado")
+        break
+    if table[1] == table[4] == table[7] == user_option:
+        print("has ganado")
+        break
+    if table[2] == table[5] == table[8] == user_option:
+        print("has ganado")
+        break
+    
+    if table[0] == table[4] == table[8] == user_option:
+        print("has ganado")
+        break
+
+
+#bot
+
     pcp = random.choice(lista)
-    table[pcp] = "O"
+    table[pcp] = pc_option
     lista.remove(pcp)
 
-    print(lista)
-
+#display decision
     print(
     "| "+table[0]+"| "+table[1]+"| "+table[2]+"|\n"
     "| "+table[3]+"| "+table[4]+"| "+table[5]+"|\n"
     "| "+table[6]+"| "+table[7]+"| "+table[8]+"|\n"
     )
+
+#bot rules
+    if table[0] == table[1] == table[2] == pc_option:
+        print("Has perdido")
+        break
+    if table[3] == table[4] == table[5] == pc_option:
+        print("Has perdido")
+        break
+    if table[6] == table[7] == table[8] == pc_option:
+        print("Has perdido")
+        break
+
+    if table[0] == table[3] == table[6] == pc_option:
+        print("Has perdido")
+        break
+    if table[1] == table[4] == table[7] == pc_option:
+        print("Has perdido")
+        break
+    if table[2] == table[5] == table[8] == pc_option:
+        print("Has perdido")
+        break
+    
+    if table[0] == table[4] == table[8] == pc_option:
+        print("Has perdido")
+        break
+
+
+
