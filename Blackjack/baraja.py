2ff7fd7d92df0ea1852229207ca378b3fde897dc
unknown <2btc11@IESRAMONLLULL.LOCAL>
2023-02-13 09:47:26 +0100

    hemos creado clase baraja

--- Blackjack/baraja.py
+++ Blackjack/baraja.py
@@ -1,5 +1,5 @@
 import random
-from carta import Carta
+from main import Carta
 
 class Baraja:
     def __init__(self):
@@ -12,9 +12,17 @@ class Baraja:
             for valor in valores:
                 cartanueva = Carta(palo, valor)
                 self.cartas.append(cartanueva)
+                
     def mezclar (self):
         random.shuffle(self.cartas)
 
+    def contar(self):
+        return len(self.cartas)
+
 mibaraja = Baraja() 
+"""
 mibaraja.mezclar()
 print(mibaraja.cartas)
+"""
+numero_cartas = mibaraja.contar()
+print(numero_cartas)
